09e7545b2a2b051d3719ab4141d84d8293606a10
baaayh <baaaayh@gmail.com>
2024-08-28 11:21:13 +0900

    imagein modified.

--- gulpfile.js
+++ gulpfile.js
@@ -19,21 +19,21 @@ const CONFIG = {
     workspace: {
         HTML: './src/',
         ASSETS: {
-            FONTS: './src/' + ASSETS + 'fonts',
-            IMAGES: './src/' + ASSETS + 'images',
-            STYLE: './src/' + ASSETS + 'scss',
-            SCRIPT: './src/' + ASSETS + 'script',
-            LIBRARY: './src/' + ASSETS + 'library',
+            FONTS: `./src/${ASSETS}fonts`,
+            IMAGES: `./src/${ASSETS}images`,
+            STYLE: `./src/${ASSETS}scss`,
+            SCRIPT: `./src/${ASSETS}script`,
+            LIBRARY: `./src/${ASSETS}library`,
         },
     },
     deploy: {
         HTML: HTML,
         ASSETS: {
-            FONTS: './dist/' + RESOURCES + ASSETS + 'fonts',
-            IMAGES: './dist/' + RESOURCES + ASSETS + 'images',
-            STYLE: './dist/' + RESOURCES + ASSETS + 'css',
-            SCRIPT: './dist/' + RESOURCES + ASSETS + 'script',
-            LIBRARY: './dist/' + RESOURCES + ASSETS + 'library',
+            FONTS: `./dist/${RESOURCES}${ASSETS}fonts`,
+            IMAGES: `./dist/${RESOURCES}${ASSETS}images`,
+            STYLE: `./dist/${RESOURCES}${ASSETS}css`,
+            SCRIPT: `./dist/${RESOURCES}${ASSETS}script`,
+            LIBRARY: `./dist/${RESOURCES}${ASSETS}library`,
         },
     },
 };
@@ -41,127 +41,103 @@ const CONFIG = {
 /**
  * ? @task : EJS
  */
-function EJS() {
-    return new Promise((resolve) => {
-        src([CONFIG.workspace.HTML + '/**/*.html', '!' + CONFIG.workspace.HTML + '/**/include/*.html', '!' + CONFIG.workspace.HTML + '/php/**/*.html'])
+async function EJS() {
+    return new Promise((resolve, reject) => {
+        src([`${CONFIG.workspace.HTML}/**/*.html`, `!${CONFIG.workspace.HTML}/**/include/*.html`, `!${CONFIG.workspace.HTML}/php/**/*.html`])
             .pipe(ejs())
-            .pipe(dest(CONFIG.deploy.HTML));
-        resolve();
+            .pipe(dest(CONFIG.deploy.HTML))
+            .on('end', resolve)
+            .on('error', reject);
     });
 }
 
 /**
  * ? @task : Sass
  */
-function CompileCSS() {
-    return new Promise((resolve) => {
-        src(CONFIG.workspace.ASSETS.STYLE + '/*.scss')
-            .pipe(
-                sass({
-                    outputStyle: 'expanded',
-                    // expanded ,compressed
-                }).on('error', sass.logError)
-            )
+async function CompileCSS() {
+    return new Promise((resolve, reject) => {
+        src(`${CONFIG.workspace.ASSETS.STYLE}/*.scss`)
+            .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
             .pipe(dest(CONFIG.deploy.ASSETS.STYLE))
-            .on('end', resolve); // 이벤트가 종료될 때만 resolve() 호출
+            .on('end', resolve)
+            .on('error', reject);
     });
 }
 
 /**
  * ? @task : Imagemin
  */
-function Imagemin() {
-    return new Promise((resolve) => {
-        src(CONFIG.workspace.ASSETS.IMAGES + '/**/*.*')
-            .pipe(newer(CONFIG.deploy.ASSETS.IMAGES))
-            .pipe(
-                imagemin([
-                    imagemin.gifsicle({ interlaced: false }),
-                    imagemin.mozjpeg({ quality: 95, progressive: false }),
-                    imagemin.optipng({ optimizationLevel: 5 }),
-                    // imagemin.svgo({
-                    //     plugins: [{ removeViewBox: false }, { cleanupIDs: false }],
-                    // }),
-                ])
-            )
-            .pipe(dest(CONFIG.deploy.ASSETS.IMAGES));
-        resolve();
+async function Imagemin() {
+    return new Promise((resolve, reject) => {
+        src(`${CONFIG.workspace.ASSETS.IMAGES}/**/*.*`, { encoding: false })
+            .pipe(imagemin([imagemin.gifsicle({ interlaced: true }), imagemin.mozjpeg({ quality: 85, progressive: true }), imagemin.optipng({ optimizationLevel: 5 }), imagemin.svgo({ plugins: [{ removeViewBox: false }, { cleanupIDs: false }] })]))
+            .on('data', (file) => {
+                console.log(`Processing file: ${file.path}`);
+            })
+            .pipe(dest(CONFIG.deploy.ASSETS.IMAGES))
+            .on('end', resolve)
+            .on('error', reject);
     });
 }
-
 /**
  * ? @task : Copy - Library
  */
-function Library() {
-    return new Promise((resolve) => {
-        src(CONFIG.workspace.ASSETS.LIBRARY + '/**/*').pipe(dest(CONFIG.deploy.ASSETS.LIBRARY));
-        resolve();
+async function Library() {
+    return new Promise((resolve, reject) => {
+        src(`${CONFIG.workspace.ASSETS.LIBRARY}/**/*`).pipe(dest(CONFIG.deploy.ASSETS.LIBRARY)).on('end', resolve).on('error', reject);
     });
 }
 
 /**
  * ? @task : Copy - Javascript
  */
-function Script() {
-    return new Promise((resolve) => {
-        src(CONFIG.workspace.ASSETS.SCRIPT + '/**/*')
-            .pipe(
-                babel({
-                    presets: ['@babel/preset-env'],
-                })
-            )
-            .pipe(dest(CONFIG.deploy.ASSETS.SCRIPT));
-        resolve();
+async function Script() {
+    return new Promise((resolve, reject) => {
+        src(`${CONFIG.workspace.ASSETS.SCRIPT}/**/*.js`)
+            .pipe(babel({ presets: ['@babel/preset-env'] }))
+            .pipe(dest(CONFIG.deploy.ASSETS.SCRIPT))
+            .on('end', resolve)
+            .on('error', reject);
     });
 }
 
 /**
  * ? @task : Copy - Fonts
  */
-function Font() {
-    return new Promise((resolve) => {
-        src(CONFIG.workspace.ASSETS.FONTS + '/**/*', { encoding: 'binary' })
-            .pipe(newer(CONFIG.deploy.ASSETS.FONTS, { encoding: 'binary' }))
-            .pipe(dest(CONFIG.deploy.ASSETS.FONTS, { encoding: 'binary' }));
-        resolve();
+async function Font() {
+    return new Promise((resolve, reject) => {
+        src(`${CONFIG.workspace.ASSETS.FONTS}/**/*`, { encoding: 'binary' }).pipe(newer(CONFIG.deploy.ASSETS.FONTS)).pipe(dest(CONFIG.deploy.ASSETS.FONTS)).on('end', resolve).on('error', reject);
     });
 }
 
 /**
  * ? @task : Clean
  */
-function Clean() {
-    return new Promise((resolve) => {
-        del.sync('./dist');
-        resolve();
-    });
+async function Clean() {
+    return del('./dist');
 }
 
 /**
  * ? @task : Browser Sync
  */
-function BrowserSync() {
-    return new Promise((resolve) => {
-        browserSync.init({
-            server: {
-                baseDir: './dist',
-                index: openFile,
-            },
-            port: 8080,
-            cors: true,
-            online: true,
-        });
-        resolve();
+async function BrowserSync() {
+    browserSync.init({
+        server: {
+            baseDir: './dist',
+            index: openFile,
+        },
+        port: 8080,
+        cors: true,
+        online: true,
     });
 }
 
 /**
  * ? @task : Lottie
  */
-function Lottie() {
-    return new Promise((resolve) => {
-        src(CONFIG.workspace.ASSETS.IMAGES + '/*.json').pipe(dest(CONFIG.deploy.ASSETS.IMAGES));
-        resolve();
+async function Lottie() {
+    return new Promise((resolve, reject) => {
+        src(`${CONFIG.workspace.ASSETS.IMAGES}/*.json`).pipe(dest(CONFIG.deploy.ASSETS.IMAGES)).on('end', resolve).on('error', reject);
     });
 }
 
@@ -169,18 +145,19 @@ function Lottie() {
  * ? @task : Watch
  */
 function Watch() {
-    watch(CONFIG.workspace.HTML + '**/**/*.html', EJS).on('change', browserSync.reload);
-    watch(CONFIG.workspace.HTML + '**/*.html', EJS).on('change', browserSync.reload);
-    watch(CONFIG.workspace.HTML + '*.html', EJS).on('change', browserSync.reload);
-    watch(CONFIG.workspace.ASSETS.STYLE + '/**/*.scss', CompileCSS).on('change', browserSync.reload);
-    watch(CONFIG.workspace.ASSETS.IMAGES + '/**/**/*.*', Imagemin);
-    watch(CONFIG.workspace.ASSETS.SCRIPT + '/**/*.js', Script).on('change', browserSync.reload);
-    watch(CONFIG.workspace.ASSETS.IMAGES + '/*.json', Lottie).on('change', browserSync.reload);
+    watch(`${CONFIG.workspace.HTML}/**/*.html`, EJS).on('change', browserSync.reload);
+    watch(`${CONFIG.workspace.ASSETS.STYLE}/**/*.scss`, CompileCSS).on('change', browserSync.reload);
+    watch(`${CONFIG.workspace.ASSETS.IMAGES}/**/*.*`, Imagemin).on('change', browserSync.reload);
+    watch(`${CONFIG.workspace.ASSETS.SCRIPT}/**/*.js`, Script).on('change', browserSync.reload);
+    watch(`${CONFIG.workspace.ASSETS.IMAGES}/*.json`, Lottie).on('change', browserSync.reload);
 }
 
-const defaultTasks = [CompileCSS, EJS, Script, Library, Font, Imagemin, Lottie, BrowserSync, Watch];
+/**
+ * ? 기본 작업
+ */
+const defaultTasks = series(Clean, parallel(CompileCSS, EJS, Script, Library, Font, Imagemin, Lottie), BrowserSync, Watch);
 
 module.exports = {
-    default: series(defaultTasks),
-    clean: series(Clean),
+    default: defaultTasks,
+    clean: Clean,
 };
